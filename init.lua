4889771b9f911b959f8817458ac0c5f57d052fc7
Collin Nigro <collin.nigro@workday.com>
2024-09-06 15:13:48 -0600

    WIP

--- init.lua
+++ init.lua
@@ -308,6 +308,13 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'startup-nvim/startup.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('startup').setup()
+    end,
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
