a41c477a61b0269697a324328aaca5021753823f
collin nigro <collinnigro@gmail.com>
2024-12-28 11:42:02 -0700

    rose-pine switch

--- init.lua
+++ init.lua
@@ -240,12 +240,11 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'rose-pine/neovim',
+    name = 'rose-pine',
     priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'catppuccin-macchiato'
-      vim.cmd.hi 'Comment gui=none'
+    config = function()
+      vim.cmd 'colorscheme rose-pine'
     end,
   },
   {
@@ -305,53 +304,7 @@ require('lazy').setup({
         violet = '#c6a0f6',
         grey = '#494d64',
       }
-
-      local bubbles_theme = {
-        normal = {
-          a = { fg = colors.black, bg = colors.violet },
-          b = { fg = colors.white, bg = colors.grey },
-          c = { fg = colors.white },
-        },
-
-        insert = { a = { fg = colors.black, bg = colors.blue } },
-        visual = { a = { fg = colors.black, bg = colors.cyan } },
-        replace = { a = { fg = colors.black, bg = colors.red } },
-
-        inactive = {
-          a = { fg = colors.white, bg = colors.black },
-          b = { fg = colors.white, bg = colors.black },
-          c = { fg = colors.white },
-        },
-      }
-      require('lualine').setup {
-        options = {
-          theme = bubbles_theme,
-          component_separators = '',
-          section_separators = { left = '', right = '' },
-        },
-        sections = {
-          lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-          lualine_b = { 'filename', 'branch' },
-          lualine_c = {
-            '%=', --[[ add your center compoentnts here in place of this comment ]]
-          },
-          lualine_x = {},
-          lualine_y = { 'filetype', 'progress' },
-          lualine_z = {
-            { 'location', separator = { right = '' }, left_padding = 2 },
-          },
-        },
-        inactive_sections = {
-          lualine_a = { 'filename' },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { 'location' },
-        },
-        tabline = {},
-        extensions = {},
-      }
+      require('lualine').setup {}
     end,
   },
   {
